ea5810b8fa618da9523fa854eb1f34d4482e90e9
Liza <semlizan@mail.ru>
2016-06-21 00:40:11 +1000

    fourth

--- vector.cpp
+++ vector.cpp
@@ -3,161 +3,154 @@
 using namespace std;
 
 template <typename T>//шаблонный класс вектор
-class Vector{
-	T * array;
-	int size;
-	int capacity;
+class Vector{//класс вектор 
+	T * array;//массив может быть любого типа 
+	int size;//размер 
+	int capacity;//вместимость
 public:
-typedef T* iterator;
-	iterator begin() { return array; }
-	iterator end() { return array + size; }
+	typedef T* iterator;//объявляем итератор именем типа 
+	iterator begin() { return array; }//указытель на первый элемент
+	iterator end() { return array + size; }//указатель  на воображаемый несуществующий элемент, следующий за последним.
 
 	Vector() : capacity(0), size(0) {//конструктор по умолчанию
-		array = (T*)(operator new (0));
+		array = (T*)(operator new (0));//выделяет память для массива 
 	}
-	Vector(int size) : Vector(size, T())//Конструктор, позволяющий задаватьразмерность вектора, но не являющийся конструктором преобразования
-                                 
-	{}
+	Vector(int size) : Vector(size, T()){}//Конструктор, позволяющий задавать размерность вектора, но не являющийся конструктором преобразования
+	
 	Vector(int _size, const T& value)//конструктор копирования из одного вектора в другой 
 	{
-		sizeA = _size;
-		capacity = size;
-		array = (T*)(operator new (sizeof(T)* capacity));
+		size = _size;//задаем размер массива
+		capacity = size;//задаем вместимость
+		array = (T*)(operator new (sizeof(T)* capacity));//выделяем память для массива, с размером и вместимостью
 		for (int i = 0; i < size; i++){
-			new (array + i)T(value);
+			new (array + i)T(value);//кладем в массив значения
 		}
 	};
-	Vector<T> & pushBack(const T& value){
+	Vector<T> & pushBack(const T& value){//добавление элементов
 		if (size == capacity)
-			reserve();
+			reserve();//увеличиваем массив
 
 		array[size++] = value;
-		return *this;
+		return *this;//указатель на текущий вектор 
 	}
-	unsigned int _size() const {
+	unsigned int _size() const {//возвращает размер
 		return (unsigned int)size;
 	}
-	unsigned int _capaciy() const {
+	unsigned int _capaciy() const {//возвращает вместимость
 		return (unsigned int)capaciy;
 	}
-	Vector<T> & popBack()
+	Vector<T> & popBack()//уделение элементов
 	{
-		(array + —size)->~T();
+		(array + —size)->~T();//вызываем диструктор для конкретного элемента
 		return *this;
 	}
 	Vector<T> & clear(){
 		for (int i = 0; i < size; i++)
-			(array + i)->~T();
+			(array + i)->~T();//удалили элементы
 
-		delete(array);
+		delete(array);//удалили массив
 
-		array = (T*)(operator new (0));
+		array = (T*)(operator new (0));//выделяем пустой массив
 		size = capacity = 0;
 		return *this;
 	}
-	~Vector(){
+	~Vector(){//деструктор на удаление массива
 		delete (array);
 	}
-	T& operator[] (int i){
+	T& operator[] (int i){//обращение по индексу
 		return array[i];
 	}
-	bool empty() const{
+	bool empty() const{//обнуляем размер
 		return size == 0;
 	}
 
-void erase(iterator it){
+void erase(iterator it){//удаление одного элементов
 		if (it < begin() || end() <= it)
-			throw nullptr;
+			throw nullptr;//бросает 0
 		for (int i = (int)(it - array); i < size; i++)
-			array[i] = array[i + 1];
-		popBack();
+			array[i] = array[i + 1];//сдвигаем
 	}
 
-void erase(iterator it, int n)
+void erase(iterator it, int n)//удаляем н эелементов
 {
 	if (n == 1)
 	{
-		erase(it);
+		erase(it);//удаление одного элементов
 		return;
 	}
 	int i = 0;
-	for (Vector<T>::iterator iterator1 = it; iterator1 != it; iterator1++, i++);
+	//for (Vector<T>::iterator iterator1 = it; iterator1 != it; iterator1++, i++);
 	for (i; i < n; i++);
 	erase(it, array + i);
 }
 
 void erase(iterator it_begin, iterator it_end)
 {
-	if (it_begin < begin() || end() <= it_begin)
+	if (it_begin < begin() || end() <= it_begin)//итератор должен находится посередине
 		throw nullptr;
 
 	if (it_end < begin() || end() <= it_end)
 		throw nullptr;
 
-	int i = (int)(it_begin - array),
-		j = (int)(it_end - array);
+	int i = (int)(it_begin - array);//вычитаем указатели
+	int	j = (int)(it_end - array);//вычитаем указатели
 
 	if (i == j)
 	{
-		erase(it_begin);
+		erase(it_begin); // удаление одного элементов
 		return;
 	}
 	j++;
 
 	while (j < size)
 	{
-		*(array + i) = *(array + j);
+		*(array + i) = *(array + j);//стираем элементы с двух сторон
 		i++;
 		j++;
 	}
-
-	int _size = i;
-	for (i++; i < size; i++)
-	{
-		(array + i)->~T();
-	}
-
-	sizeArray = _size;
 }
 
-void insert(const iterator it, T value)
+void insert(const iterator it, T value)//вставка
 {
 	if (it < begin() || end() <= it)
 		throw nullptr;
 
-	Vector<T>::iterator it1 = it;
+	Vector<T>::iterator it1 = it;//новый итератор
 	if (size == capacity)
 	{
-		int i = (int)(it1 - array);
-		reserve();
+		int i = (int)(it1 - array);//вычитаем указатели
+		reserve();//добавляем памяти
 		it1 = array + i;
 	}
 
-	new (end())T();
+	new (end())T();//новый итератор
 
 	for (Vector<T>::iterator i = end(); i != it1; i--)
-		*i = *(i - 1);
+		*i = *(i - 1);//стираем с двух концов
 
-	*it1 = value;
+	*it1 = value;//кладем в вектор значение с помощью вектора 
 	size++;
 	return;
 }
 
-void reserve()
+void reserve()//увеличиваем массива
 {
-	capacity == 0 ? capacity = 2 : capacity *= 2;
-	T *newArray = (T*)(operator new (sizeof(T)* capacity));
+	if (capacity == 0){//если вместимость равна нулю, то надо увеличить
+		capacity = 2; 
+	}
+	else capacity *= 2;
+	T *newArray = (T*)(operator new (sizeof(T)* capacity));//создали новый массив
 
 	for (int i = 0; i < size; i++)
-		new(newArray + i)T(array[i]);
+		new(newArray + i)T(array[i]);//заполняем новый массив элементами
 
 	for (int i = 0; i < size; i++){
-		array[i].~T();
+		array[i].~T();//удаляем элементы старого массива
 	}
-	array = newArray;
+	array = newArray;//приравниваем массивы
 }
 
-void swap(Vector<T>& vector)
+void swap(Vector<T>& vector)//меняем местами
 {
 	swap(array, vector.array);
 	swap(size, vector.size);
@@ -167,5 +160,4 @@ void swap(Vector<T>& vector)
 
 };
 int main(){
-
 }
